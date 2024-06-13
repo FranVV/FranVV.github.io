3340b2e7a96fbd227de8c54a6ef7fee9438dee4c
Fran <33669195+FranVV@users.noreply.github.com>
2024-06-13 19:01:21 +0200

    Add files via upload

---
+++ lib/bootstrap-5.0.2-dist/css/index.php
@@ -0,0 +1,21 @@
+<?php
+    session_start();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>css</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <?php
+
+    ?>
+</head>
+<body>
+    <?php
+        header('Location: ../../index.php');
+    ?>
+</body>
+</html>
