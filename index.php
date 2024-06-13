71a8a4687bed4db4f53d69ab781d5430305848b5
Fran <33669195+FranVV@users.noreply.github.com>
2024-06-13 17:09:03 +0200

    asd (#1)
    asdadsasdasd

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Osint</title>
+        <?php 
+            require_once('header/header.php');  
+        ?>  
+    </head>
+    <body>
+        <?php
+            require_once('header/nav/nav.php');
+        ?>
+        
+    </body>
+</html>
